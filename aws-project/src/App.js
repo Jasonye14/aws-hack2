ac54b94c14a02aef808592e7df5a19c94a25b6ae
Alec Jang <ayj.max@gmail.com>
2023-11-11 22:40:21 -0500

    fixed google air polution bug

--- aws-project/src/App.js
+++ aws-project/src/App.js
@@ -7,14 +7,30 @@ import MainPage from './pages/main';
 function App() {
   const [data, setData] = useState({});
 
+  // "X-Goog-User-Project": "le-laf-hackathon"
+  // "Authorization": "Bearer ya29.a0AfB_byCSVcblirmXsPRVDx9o3H9Idp_vgBUzAvZKEmUcNqfnnVmyJRPfAODc6iXDYhv9tveytzlVMgG7XXqqgtropD59L7SKFKbkVctA_W5bgOQqfQyWq_xMvDbkcZQIzFagjKw0Q9Fzf6NjZVqlVGLc5qpoTYg2LtwuaCgYKAe8SARASFQHGX2Mi3QkNvR00uSLJ3Unc89b4gQ0171",
   useEffect(() => {
-
     fetch("https://airquality.googleapis.com/v1/currentConditions:lookup?key=AIzaSyACtvFhkIF7nUaCXpLiZmpauqJG5mTPaB4", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      mode: "no-cors"
+      headers: { 
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        universalAqi: true,
+        location: {
+            latitude: 37.419734,
+            longitude: -122.0827784
+        },
+        extraComputations: [
+            "HEALTH_RECOMMENDATIONS",
+            "DOMINANT_POLLUTANT_CONCENTRATION",
+            "POLLUTANT_CONCENTRATION",
+            "LOCAL_AQI",
+            "POLLUTANT_ADDITIONAL_INFO"
+        ],
+        languageCode: "en"
+      })
     }).then(res => res.json()).then(data => { console.log(data); });
-
   }, []);
 
   return (
